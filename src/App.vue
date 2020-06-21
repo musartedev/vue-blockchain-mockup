e6612fbbae78d0241ba247f441c611263c26b4a5
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-20 21:54:03 -0500

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { PxHeader }
 };
 </script>
 
