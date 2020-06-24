92c02ebb912e66b69b47e9f126675ddd810866ed
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-24 14:09:10 -0500

    Add about and error page

--- src/router.js
+++ src/router.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home";
+import About from "@/views/About";
+import Error from "@/views/Error";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
 
-  routes: [{ path: "/", name: "home", component: Home }]
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About
+    },
+    {
+      path: "*",
+      name: "error",
+      component: Error
+    }
+  ]
 });
---
+++ src/views/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">
+      Error 404
+    </h1>
+    <p class="text-gray-600 text-xl text-center">
+      Are you lost?
+    </p>
+    <p class="text-gray-600 text-xl text-center">You can catch me on Twitter</p>
+    <a
+      class="text-xl text-center"
+      href="https://twitter.com/musartedev"
+      target="_blank"
+      >@musartedev</a
+    >
+    <router-link class="mt-20 text-xl text-center" to="/"
+      >Or you can go back to home</router-link
+    >
+  </div>
+</template>
